197c1336181c370a0b48351c4d3ca69ff0f2454d
DespairP <328060206@qq.com>
2021-05-02 02:49:29 +0800

    Initial commit

---
+++ src/main/java/teamHTBP/LaLuzdelAlba/LaLuzdelAlba.java
@@ -0,0 +1,11 @@
+package teamHTBP.LaLuzdelAlba;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("laluzdelalba")
+public class LaLuzdelAlba {
+    public final static Logger LOGGER = LogManager.getLogger();
+
+}
